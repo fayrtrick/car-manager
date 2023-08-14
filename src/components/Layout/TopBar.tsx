a1b09dada3699e9fb52e9a837bb1d700e5070121
Tom <tom.depasse@gmail.com>
2023-08-14 18:12:59 +0200

    Modification du logo

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -2,14 +2,14 @@ import { HiMenu } from "react-icons/hi";
 
 export function TopBar() {
   return (
-    <div className="w-full py-2 px-3 flex justify-between items-center bg-primary-400 drawer-button lg:hidden border-0">
+    <div className="w-full py-2 px-3 flex justify-between items-center bg-primary-400 drawer-button lg:hidden border-0 relative">
       <label
         htmlFor="sidebar"
         className="hover:text-primary-100 duration-100 p-3 cursor-pointer text-white"
       >
         <HiMenu size={26} />
       </label>
-      <button className="p-3 text-white">
+      <button className="p-3 text-white absolute left-1/2 -translate-x-1/2">
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="currentColor"
              stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
              className="lucide lucide-car">
