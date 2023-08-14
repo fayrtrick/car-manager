89a6008e7bc615c7fca3816541607d305a899408
Tom <tom.depasse@gmail.com>
2023-08-14 17:46:41 +0200

    Avatar dans la sidenav

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -15,16 +15,20 @@ const liensNavigation = [
 
 const Sidebar = ({}: SidebarProps) => {
   return (
-    <div className="drawer-side">
+    <div className="drawer-side shadow-md">
       <label htmlFor="sidebar" className="lg:!invisible drawer-overlay"></label>
 
-      {/* Contenu */}
       <div className="flex flex-col h-full w-80 bg-white">
 
-        <div className="lg:hidden flex justify-end p-3">
+        <div className="lg:hidden flex justify-end p-4 pb-0">
           <label htmlFor="sidebar" className="p-1.5 cursor-pointer">
             <GrClose className="!stroke-gray-100" />
           </label>
+        </div>
+
+        <div className="flex flex-col items-center px-4 py-6 gap-2 border-b">
+          <div className="font-bold text-xl bg-base-100 w-14 h-14 rounded-full flex justify-center items-center">JD</div>
+          <p className="font-medium">John Doe</p>
         </div>
 
         <ul className="menu p-4 w-80 h-full text-base-content">
