73f7732be26d84fb02ec3f618b7e75ec58ebbfef
fayrtrick <sylvain.bouillon@live.be>
2023-08-14 17:03:17 +0200

    initialisation de daisyui + début drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import Layout from "./components/Layout/Layout";
+import { useState } from "react";
+import { HiMenu } from "react-icons/hi";
+import Sidebar from "./components/Layout/Sidebar";
 
 function App() {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <div>
-      <Layout>
-        <p>test</p>
-      </Layout>
+    <div className="drawer drawer-mobile lg:drawer-open">
+      <input id="sidebar" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        <div className="w-full py-2 px-5 flex justify-between items-center bg-secondary shadow-xl drawer-button lg:hidden border-0">
+          <label
+            htmlFor="sidebar"
+            className="hover:bg-[#E2E2E2] duration-100 p-3 rounded-3xl"
+          >
+            <HiMenu className="cursor-pointer" size={30} />
+          </label>
+          <img
+            className="max-w-[80px]"
+            src="https://static.vecteezy.com/system/resources/previews/013/923/537/original/red-car-vehicle-auto-detailing-logo-png.png"
+          />
+        </div>
+        <div className="px-12 lg:px-16 py-7">
+          <p>test</p>
+        </div>
+      </div>
+      <Sidebar open={open} setOpen={setOpen} />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import daisyUi from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F9703E",
+          "primary-content": "#ffffff",
+          secondary: "#FFD0B5",
+          accent: "#98AEEB",
+          neutral: "#323F4B",
+          "base-100": "#F2F5F9",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyUi],
 };
