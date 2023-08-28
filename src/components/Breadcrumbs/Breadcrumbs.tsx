520854a74e377dea0e6fd8552bef15a5464ebd09
Tom <tom.depasse@gmail.com>
2023-08-28 22:33:34 +0200

    Breadcrumbs : 🤮 => 🧼 => 😃👍

---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,24 @@
+import Breadcrumb from "./Breadcrumb";
+
+interface BreadcrumbItem {
+  name: string;
+  href?: string;
+}
+
+interface BreadcrumbsProps {
+  breadcrumbs: BreadcrumbItem[];
+}
+
+const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
+  return (
+    <div className="text-sm breadcrumbs">
+      <ul>
+        {breadcrumbs.map((breadcrumb, index) => (
+          <Breadcrumb key={index} {...breadcrumb} last={index === breadcrumbs.length - 1} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
